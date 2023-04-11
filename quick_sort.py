a2c87256a66f2a967e4fc194328fab862631d3c8
Maxim Afanasiev <125731078+makeas231@users.noreply.github.com>
2023-04-11 22:10:37 +0300

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,8 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
